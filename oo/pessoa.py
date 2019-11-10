3ac521ef9d9577886999bdae9a11d792ad692679
55279 <anselmonnvix@yahoo.com>
2019-11-09 23:22:09 -0300

    Sobrescrita de atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,11 +17,15 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos{cls.olhos}'
 
+class Homem(Pessoa): #herança - Classe Homem é um filho da classe Pessoa e herdou todos os métodos e atributos
+    pass
 
+class Mutante(Pessoa):
+    olhos = 3
 
 if __name__ == '__main__':
-    renzo = Pessoa(nome='Renzo')
-    luciano = Pessoa(renzo, nome='Luciano')
+    renzo = Mutante(nome='Renzo')
+    luciano = Homem(renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -38,3 +42,11 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.metodo_estatico())
+
+    #Conseitos de Herança
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    print(renzo.olhos)
